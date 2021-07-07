7d8a50cbc89b571011e90b25e3b03325a012f1a3
vicgom7 <victormgomezga@gmail.com>
2021-07-06 19:07:52 -0500

    primera actualizacion

---
+++ Clase1/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola, World!");
+        int x = 0;
+        x = 3;
+        System.out.println(x);
+
+
+    }
+}
+
